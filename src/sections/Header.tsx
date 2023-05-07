cf8ecaff5bc985cc323b1843aefeff3281500b91
lokmane.ghedied <lokmane.ghedied@gmail.com>
2023-05-07 20:49:53 +0100

    changes

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,7 +2,7 @@ import HireMe from "../components/HireMe";
 const Header = () => {
     return ( 
         <div className="w-full h-20 flex items-center justify-start place-self-center px-3">
-                <span className='font-extrabold cursor-pointer -space-y-3'>
+                <span className='font-extrabold cursor-pointer -space-y-3 lg:-space-y-2'>
                     <span className="text-2xl lg:text-3xl logo">Lokmane</span>
                     <h1 className='text-lg lg:text-xl'>GHEDIED</h1>
                 </span>
