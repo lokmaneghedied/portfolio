adef22768f712772f709db1d36fe71c0a76bbc8b
lokmaneghedied <lokmane.ghedied@gmail.com>
2024-10-09 15:04:31 +0100

    Updates

---
+++ assets/TitleTicket-ad8685dc.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-254de44f.js";function i({title:r,Icon:t}){return e.jsxs("div",{className:"px-4 py-2 rounded-full border border-grayBorder flex items-center justify-center gap-x-3 text-[#9D9D9F] w-fit",children:[t,e.jsx("p",{className:"text-base font-semibold",children:r})]})}export{i as T};
